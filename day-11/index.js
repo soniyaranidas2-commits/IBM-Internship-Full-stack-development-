065c204565bb6a2684db49543123aa7d99b3f928
Soniya Rani Das <soniyaranidas2@gmail.com>
2026-07-20 19:39:35 +0530

    New changes

---
+++ day-11/index.js
@@ -0,0 +1,139 @@
+// let out = myFun(23, 45, 56);
+
+
+// console.log(out)
+
+
+
+// function myFun(){
+
+
+//     let sum = 100
+
+
+//     console.log(sum)
+
+
+// }
+
+
+// myFun()
+
+
+// console.log(sum)
+
+
+
+// console.log(name)
+
+
+// var name = "Praljyoti"
+
+
+
+// block scop, global scop
+
+
+// console.log(myName)             //myName = undefined
+
+
+// let myName = "Rabi"             //myName="Assam"
+
+
+// myName = "Assam"
+
+
+// console.log(myName)
+
+
+//IIFE
+
+
+// (()=>{
+//     console.log("IIFE")
+// })()
+
+
+
+//JS property Hoisting, all traditional functions, var
+//Execution context
+
+
+//Higher order function
+
+
+
+//forEach, map, filter, reduce
+
+
+//They all will have arrays as input
+
+
+// let arr = [1,2,3,4,5,6,7,8,9]
+
+
+
+//forEach
+
+
+// arr.forEach((e, i)=>{
+//     console.log(e*4)
+// })
+
+
+
+// //map
+
+
+// let mapOut = arr.map((e, i)=>{
+//     return e*2
+// })
+
+
+// console.log(mapOut)
+
+
+//filter
+
+
+// let filterOut = arr.filter((e, i)=>{
+//     return e%2!==0
+// })
+
+
+// console.log(filterOut)
+
+
+
+
+
+// console.log(reduceOut)
+
+
+
+// let arr1 = [2,4,6,8,10,3,5,7,9]
+
+
+
+// let out = arr.map((el, i)=>{
+//     return el*3
+// }).filter((el, i)=>{
+//     return el%5==0
+// }).forEach((el, i)=>{
+//     console.log(el*200)
+// })
+
+
+// console.log(out)
+
+
+
+// let arr = [1,2,3,4,5]
+
+
+// let out = arr.reduce((acc, el)=>{
+//     return acc+el
+// })
+
+
+// console.log(out)
